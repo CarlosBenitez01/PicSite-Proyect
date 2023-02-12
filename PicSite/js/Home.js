db12c1078d9256157331c18a160733dfaa297bd7
CarlosBenitez01 <87823966+CarlosBenitez01@users.noreply.github.com>
2023-02-12 09:36:50 -0600

    PicSite
    Login, Registro y Home(Sin terminar)

---
+++ PicSite/js/Home.js
@@ -0,0 +1,7 @@
+/*
+var buscador = window.getComputedStyle(document.querySelector('.buscador:focus'));
+var lupa = document.getElementsByClassName("buscador-ico");
+lupa[0].style.filter="none";
+*/   
+
+
